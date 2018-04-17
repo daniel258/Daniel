79c25430c95d6b577a323dc81f9da6803b3db709
daniel258 <danielnevo@gmail.com>
2018-04-17 09:47:34 -0400

    Added and exported CatIndex

--- R/CatIndex.R
+++ R/CatIndex.R
@@ -1,6 +1,16 @@
-# useful for simulations: CatIndex print on screen the infex index.var and its value
+#
+#' CatIndex Function
+#'
+#' Useful for simulations: CatIndex print on screen the infex index.var and its value
+#' @param index.var  index.char
+#' @keywords simulations
+#' @export
+#' @examples
+#' i = 7
+#' CatIndex(i)
+#' CatIndex(i,"index i")
 CatIndex <- function(index.var, index.char = NULL)
 {
   if(missing(index.char)) { index.char <- deparse(substitute(index.var))}
-  cat(paste(index.char,"= "), index.var, "\n")
+  cat(paste(index.char,"="), index.var, "\n")
 }
