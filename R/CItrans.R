b5c4a99a13f6b80b7cd332536910e01dcadf0a66
daniel258 <danielnevo@gmail.com>
2018-04-17 10:04:04 -0400

    Added CItrans function

---
+++ R/CItrans.R
@@ -0,0 +1,14 @@
+#' CItrans Function
+#'
+#' CItrans takes coef estimate, its estimated se, calculates CI according to normal asusmption, and then transormate the ci according to the function Ftrans
+#' @param coef se Ftrans cover
+#' @export
+#' @examples
+#'
+CItrans <- function(coef, se, Ftrans, cover = 0.95)
+{
+  alpha <- 1- cover
+  ci.no.trans <- coef + c(-1,1)*qnorm(1- alpha/2)*se
+  ci <- do.call(Ftrans, list(x = ci.no.trans))
+  return(ci)
+  }
